f77b66cc83ef84dea886f7885ff9c6494d4c8374
FlippeRRG <QWeAsdZxc123008>
2018-07-11 16:06:33 +0300

    FizzBuzz

--- IdeaTest1/src/variousMethods.java
+++ IdeaTest1/src/variousMethods.java
@@ -38,7 +38,20 @@ public class variousMethods {
         sum = sum * sum;
         int difference = sum - sumSquare;
         return difference;
+    }
 
+    private static void fizzBuzz (int begin, int end){
+        for (int i = begin; i < end; i++) {
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuZZ");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -53,5 +66,7 @@ public class variousMethods {
         System.out.println(sumSquareDifference( 10 ));
         System.out.println(sumSquareDifference( 5 ));
         System.out.println(sumSquareDifference( -15 ));
+
+        fizzBuzz( 1, 100 );
     }
 }
